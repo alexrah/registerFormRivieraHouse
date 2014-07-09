11c21110abaa26ad7876536980f44cb25381f056
Ale Sandro <alexrah@gmail.com>
2014-07-09 14:32:09 +0200

    change server port to 9090 - start edit AJAX validate

--- form_logic.js
+++ form_logic.js
@@ -12,10 +12,31 @@ document.formvalidator.setHandler('length11',
 document.formvalidator.setHandler('surname',
   function (value){
     
+// var json = (function(){
+// var json = null;
+$.ajax({
+  'url': "surname-condos_list_rivierahouse.json",
+  'async': "false",
+  'dataType': "json",
+  'global': "false",
+  'success': "data"
+   
+    // console.log(data);
+ 
+    // console.log(json);
+  
+});
+// return json;
+// })();
 
+    console.log(data);
+for(i=0; i< json.count; i++){
+if(json[i].surname == value){
+    console.log(json[i].condos);
+  }
+}
 
   });
-
 
 
 });
