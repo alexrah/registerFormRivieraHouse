6e12933323befb55de3317fe6f6581b6bc27b0af
alexrah <alexrah@gmail.com>
2014-07-10 17:32:30 +0200

    finished form logic - mods json to test multiple surnames - dynamic dropdown

--- form_logic.js
+++ form_logic.js
@@ -12,31 +12,51 @@ document.formvalidator.setHandler('length11',
 document.formvalidator.setHandler('surname',
   function (value){
     
-// var json = (function(){
-// var json = null;
+var json = (function(){
+var json = null;
 $.ajax({
   'url': "surname-condos_list_rivierahouse.json",
-  'async': "false",
-  'dataType': "json",
-  'global': "false",
-  'success': "data"
-   
-    // console.log(data);
- 
-    // console.log(json);
-  
+  'async': false,
+  // 'dataType': "json",
+  // 'global': "false",
+  'success': function(data){ json = { lista : data }; }
 });
-// return json;
-// })();
+return json;
+})();
 
-    console.log(data);
-for(i=0; i< json.count; i++){
-if(json[i].surname == value){
-    console.log(json[i].condos);
+// console.log( json);
+// console.log( json.lista.length);
+
+// console.log( json.lista[1].surname );
+   
+
+var matchCondo = function(json){ 
+var matchC = [];
+  for(i=0; i< json.lista.length; i++){
+    if(json.lista[i].surname == value){
+      // console.log(json.lista[i].surname);
+      // console.log(json.lista[i].condos);
+      matchC.push(json.lista[i].condos);
+    }
   }
-}
+// console.log(matchC);
+  return matchC;
+  };
+
+var trial = matchCondo(json);
+console.log(trial);
+
+ 
+
+if (trial.length > 0 ) {
+    $('#listaCondo').html("");
+  for (i=0; i< trial.length; i++){
+    $('#listaCondo').append('<option value=""></option>');
+    $('#listaCondo option').eq(i).attr("value", trial[i]  );
+    $('#listaCondo option').eq(i).text( trial[i]  );
+  }
+  return true; } else { return false; }
 
   });
-
 
 });
